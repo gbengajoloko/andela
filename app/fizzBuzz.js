390722c2c75fae314fbc9ec575b24dae09dcc35e
gbengajoloko <gbengajoloko@gmail.com>
2017-03-11 09:51:56 +0100

    fizzbuzz app

---
+++ app/fizzBuzz.js
@@ -0,0 +1,19 @@
+var exports = module.exports={};
+exports.fizzBuzz = function (num)
+{
+if (num===0){
+	return num
+}
+if (num%3===0 && num%5===0){
+	return "FizzBuzz";
+}
+else if (num%3===0){
+   return "Fizz";
+}
+else if (num%5===0){
+	return "Buzz";
+}
+else{
+	return num;
+}
+}
